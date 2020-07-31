64e649d6f81e5434b9024bf50848fe32ddc48cda
SadafHaroon2020 <sadafssuet05@gmail.com>
2020-07-31 12:02:14 -0500

    creating a starting code based on requirement

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component{
+  /*creating state for color and counter*/
+  constructor() {
+    super()
+    this.state = {
+      counter: 0,
+      color: "white",
+    }
+  }
+  /**
+   * using component lifecycle method to set state.
+   */
+  render() {
+    return (
+      <div className="App">
+        <div className="box" onClick = "">
+                
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
